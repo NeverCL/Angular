6f06dc8fc56be71ea5c8b8390f8aa0e96fda85a5
Never、C <caolin>
2016-10-01 23:48:26 +0800

    add ngResource

---
+++ Angular/Angular/Data.ashx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Angular
+{
+    /// <summary>
+    /// Data 的摘要说明
+    /// </summary>
+    public class Data : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "json/application";
+            if (!string.IsNullOrEmpty(context.Request["Id"]) || context.Request.HttpMethod == "POST")
+            {
+                var id = Guid.NewGuid();
+                context.Response.Write(JsonConvert.SerializeObject(new { Id = id, Value = "Value:" + id }));
+                return;
+            }
+
+            var list = new List<object>();
+            for (int i = 0; i < 4; i++)
+            {
+                list.Add(new { Id = i, Value = "Value:" + i });
+            }
+            //var data = new {Data = list, Count = 20};
+            context.Response.Write(JsonConvert.SerializeObject(list));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
